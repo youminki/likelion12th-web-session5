94e3b59ddaf5ad2df0b893626179e31da6499851
constantly-dev <rntqhdl10@naver.com>
2024-05-05 00:53:49 +0900

    feat: 로그인 모달 구현

---
+++ src/assets/Data.js
@@ -0,0 +1,114 @@
+import poster1 from '../assets/image/1.jpg';
+import poster2 from '../assets/image/2.jpg';
+import poster3 from '../assets/image/3.jpg';
+import poster4 from '../assets/image/4.jpg';
+import poster5 from '../assets/image/5.jpg';
+import poster6 from '../assets/image/6.jpg';
+import poster7 from '../assets/image/7.jpg';
+import poster8 from '../assets/image/8.jpg';
+import poster9 from '../assets/image/9.jpg';
+import poster10 from '../assets/image/10.jpg';
+
+export const DATA = [
+  {
+    rank: 1,
+    img: poster1,
+    title: '범죄도시 4',
+    year: '2023',
+    country: '한국',
+    average: '3.0',
+    percent: '70%',
+    audience: '6420000',
+  },
+  {
+    rank: 2,
+    img: poster2,
+    title: '쿵푸팬더 4',
+    year: '2024',
+    country: '미국',
+    average: '2.9',
+    percent: '6.6%',
+    audience: '1470000',
+  },
+  {
+    rank: 3,
+    img: poster3,
+    title: '혹성탈출: 새로운 시대',
+    year: '2024',
+    country: '미국',
+    average: '',
+    percent: '4.7%',
+    audience: '',
+  },
+  {
+    rank: 4,
+    img: poster4,
+    title: '포켓몬스터: 성도지방 이야기, 최종장',
+    year: '2001',
+    country: '일본',
+    average: '3.2',
+    percent: '3.8%',
+    audience: '39000',
+  },
+  {
+    rank: 5,
+    img: poster5,
+    title: '스턴트맨',
+    year: '2024',
+    country: '미국',
+    average: '3.3',
+    percent: '2.4%',
+    audience: '97000',
+  },
+  {
+    rank: 6,
+    img: poster6,
+    title: '악마와의 토크쇼',
+    year: '2023',
+    country: '호주',
+    average: '',
+    percent: '1.7%',
+    audience: '389',
+  },
+  {
+    rank: 7,
+    img: poster7,
+
+    title: '가필드 더 무비',
+    year: '2024',
+    country: '영국',
+    average: '',
+    percent: '1.4%',
+    audience: '411',
+  },
+  {
+    rank: 8,
+    img: poster8,
+    title: '극장판 하이큐!! 쓰레기장의 결전',
+    year: '2023',
+    country: '일본',
+    average: '',
+    percent: '1.3%',
+    audience: '694',
+  },
+  {
+    rank: 9,
+    img: poster9,
+    title: '꼬마참새 리차드: 신비한 보석 탐험대',
+    year: '2023',
+    country: '독일',
+    average: 2.8,
+    percent: '0.9%',
+    audience: '7186',
+  },
+  {
+    rank: 10,
+    img: poster10,
+    title: '남은 인생 10년',
+    year: '2022',
+    country: '일본',
+    average: '3.2',
+    percent: '0.9%',
+    audience: '440000',
+  },
+];
