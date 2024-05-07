f66e143e234a13d4790377d72afef790bb246803
youminki <dbalsrl7648@naver.com>
2024-05-08 02:01:44 +0900

    footer하단 고정

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import Footer from './page/Footer/Footer.js';
 import Header from './page/Header/Header.js';
-import Main from './page/Main';
+
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      
       <Footer />
     </>
   );
