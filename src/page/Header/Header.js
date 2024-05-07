c55c1cae4a1802bac6764ffac8541721e8ce51db
youminki <dbalsrl7648@naver.com>
2024-05-08 03:05:44 +0900

    파일 분리
    헤더의 로그인 모달과 회원가입 모달을 분리

--- src/page/Header/Header.js
+++ src/page/Header/Header.js
@@ -1,59 +1,21 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { BrowserRouter as Router, Link, Route, Routes } from "react-router-dom";
-import google from '../../assets/image/icon_google.png';
-import kakao from '../../assets/image/icon_kakao.png';
-import line from '../../assets/image/icon_line.png';
-import { default as logo, default as watcha } from '../../assets/image/icon_logo.png';
-import twitter from '../../assets/image/icon_twitter.png';
-import Modal from '../../components/Modal';
+import { default as watcha } from '../../assets/image/icon_logo.png';
 import Book from '../Book';
 import '../Header/Header.css';
+import Login from '../Login';
 import Movie from '../Movie';
 import Series from '../Series';
+import SignUp from '../SignUp';
 import Webtoon from '../Webtoon';
 
 const Footer = () => {
-  const [loginModalOpen, setLoginModalOpen] = useState(false);
-  const [signUpModalOpen, setSignUpModalOpen] = useState(false);
   const [searchTerm, setSearchTerm] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-
-  const openLoginModal = () => {
-    setLoginModalOpen(true);
-  };
-
-  const closeLoginModal = () => {
-    setLoginModalOpen(false);
-  };
-
-  const openSignUpModal = () => {
-    setSignUpModalOpen(true);
-  };
-
-  const closeSignUpModal = () => {
-    setSignUpModalOpen(false);
-  };
 
   const handleSearchChange = (event) => {
     setSearchTerm(event.target.value);
   };
 
-  useEffect(() => {
-    localStorage.setItem('email', 'dbalsrl7648@naver.com'); //로그인 아이디
-    localStorage.setItem('password', '1234'); //로그인 비밀번호
-  }, []);
-
-  const handleLogin = () => {
-    const storedEmail = localStorage.getItem('email');
-    const storedPassword = localStorage.getItem('password');
-
-    if (email === storedEmail && password === storedPassword) {
-      alert('로그인 성공');
-    } else {
-      alert('로그인 실패');
-    }
-  };
   return (
     <Router>
       <>
@@ -73,63 +35,9 @@ const Footer = () => {
             onChange={handleSearchChange}
             className="searchInput"
           />
-
-          <div className="LoginButton" onClick={openLoginModal}>로그인</div>
-          <button className="SignUpButton" onClick={openSignUpModal}>회원가입</button>
+          <Login />
+          <SignUp />
         </div>
-        <Modal open={loginModalOpen} close={closeLoginModal}>
-          <img className="LogoImage" src={logo} alt="로고 이미지" />
-          <div className="Title">로그인</div>
-          <input className="LoginInput" placeholder="이메일" onChange={(e) => setEmail(e.target.value)} />
-          <input className="LoginInput" placeholder="비밀번호" onChange={(e) => setPassword(e.target.value)} />
-          <button className="LoginRedButton" onClick={handleLogin}>로그인</button>
-          <div className="Content" style={{ color: "#ff2f6e" }}>비밀번호를 잃어버리셨나요?</div>
-          <div className="CenterBlock">
-            <div className="Content" style={{ color: "#8c8c8c" }}>계정이 없으신가요?</div>
-            <div className="Content" style={{ color: "#ff2f6e" }}>회원가입</div>
-          </div>
-          <div className="CenterBlock">
-            <div className="GrayLine" />
-            <div className="OrContent">OR</div>
-            <div className="GrayLine" />
-          </div>
-          <div className="WrapIcon">
-            <img src={kakao} alt="kako" />
-            <img src={google} alt="google" />
-            <img src={twitter} alt="twitter" />
-            <img src={line} alt="line" />
-          </div>
-          <div className="LoginTip">
-            TIP.왓챠 계정이 있으신가요? 왓챠와 왓챠피디아는 같은 계정을 사용해요.
-          </div>
-        </Modal>
-
-        <Modal open={signUpModalOpen} close={closeSignUpModal}>
-          <img className="LogoImage" src={logo} alt="로고 이미지" />
-          <div className="Title">회원가입</div>
-          <input className="LoginInput" placeholder="이름" />
-          <input className="LoginInput" placeholder="이메일" />
-          <input className="LoginInput" placeholder="비밀번호" />
-          <button className="LoginRedButton">로그인</button>
-          <div className="CenterBlock">
-            <div className="Content" style={{ color: "#8c8c8c" }}>이미 가입하셨나요?</div>
-            <div className="Content" style={{ color: "#ff2f6e" }}>로그인</div>
-          </div>
-          <div className="CenterBlock">
-            <div className="GrayLine" />
-            <div className="OrContent">OR</div>
-            <div className="GrayLine" />
-          </div>
-          <div className="WrapIcon">
-            <img src={kakao} alt="kako" />
-            <img src={google} alt="google" />
-            <img src={twitter} alt="twitter" />
-            <img src={line} alt="line" />
-          </div>
-          <div className="LoginTip">
-            TIP.왓챠 계정이 있으신가요? 왓챠와 왓챠피디아는 같은 계정을 사용해요.
-          </div>
-        </Modal>
 
         <Routes>
           <Route path="/movie" element={<Movie />} />
