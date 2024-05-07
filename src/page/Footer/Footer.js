39186dc9fc133d925f42ff4781fcfcccdd5fdc23
youminki <dbalsrl7648@naver.com>
2024-05-07 18:48:09 +0900

    Header, Footer

---
+++ src/page/Footer/Footer.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../App.css';
+import '../App.js';
+import '../Footer.css';
+
+
+function Footer() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Footer;
---
+++ src/page/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+import '../App.js';
+import '../Header.css';
+
+function Header() {
+    return (
+        <div>
+
+        </div>
+    );
+}
+
+export default Header;
