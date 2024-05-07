27063464069ce6c876093db23a8394d4ee14e5de
youminki <dbalsrl7648@naver.com>
2024-05-07 17:57:20 +0900

    Modal js파일 분리

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,6 +1,5 @@
-// ../src/components/Modal.js
-
-import styled from 'styled-components';
+import React from 'react';
+import './Modal.css';
 
 const Modal = (props) => {
   const { open, close } = props;
@@ -8,54 +7,17 @@ const Modal = (props) => {
   return (
     <>
       {open && (
-        <ModalBackground>
-          <ModalBlock>
-            <TopContent>
-              <CloseButton onClick={close}>&times;</CloseButton>
-            </TopContent>
+        <div className="ModalBackground">
+          <div className="ModalBlock">
+            <div className="TopContent">
+              <button className="CloseButton" onClick={close}>×</button>
+            </div>
             {props.children}
-          </ModalBlock>
-        </ModalBackground>
+          </div>
+        </div>
       )}
     </>
   );
 };
-
-const ModalBackground = styled.div`
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  z-index: 99;
-  background-color: rgba(0, 0, 0, 0.6);
-  text-align: center;
-`;
-
-const ModalBlock = styled.div`
-  width: 350px;
-  height: 600px;
-  margin: auto;
-  border-radius: 5px;
-  background-color: #ffffff;
-  align-items: center;
-`;
-
-const CloseButton = styled.button`
-  display: flex;
-  width: 10px;
-  font-size: 21px;
-  font-weight: 700;
-  text-align: center;
-  color: #696969;
-  background-color: transparent;
-  cursor: pointer;
-  border: white;
-`;
-
-const TopContent = styled.div`
-  position: relative;
-  font-size: 30px;
-`;
 
 export default Modal;
