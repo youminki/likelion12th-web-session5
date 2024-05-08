f560f7a3a994d792d4d266cdc2fbd6824e789613
youminki <dbalsrl7648@naver.com>
2024-05-08 16:42:38 +0900

    weebton Img, data

--- src/page/Webtoon.js
+++ src/page/Webtoon.js
@@ -1,33 +1,33 @@
 import React from 'react';
-import movie1 from '../assets/image/1.jpg';
-import movie2 from '../assets/image/2.jpg';
-import movie3 from '../assets/image/3.jpg';
-import movie4 from '../assets/image/4.jpg';
-import movie5 from '../assets/image/5.jpg';
-import movie6 from '../assets/image/6.jpg';
-import movie7 from '../assets/image/7.jpg';
-import movie8 from '../assets/image/8.jpg';
-import movie9 from '../assets/image/9.jpg';
+import webtoonImg1 from '../assets/image/webtoonImg1.jpeg';
+import webtoonImg2 from '../assets/image/webtoonImg2.jpeg';
+import webtoonImg3 from '../assets/image/webtoonImg3.jpeg';
+import webtoonImg4 from '../assets/image/webtoonImg4.jpeg';
+import webtoonImg5 from '../assets/image/webtoonImg5.jpeg';
+import webtoonImg6 from '../assets/image/webtoonImg6.jpeg';
+import webtoonImg7 from '../assets/image/webtoonImg7.jpeg';
+import webtoonImg8 from '../assets/image/webtoonImg8.jpeg';
+import webtoonImg9 from '../assets/image/webtoonImg9.jpeg';
 import '../page/Movie.css';
 
-const Webtoon = () => {
-    const movies = [
-        { src: movie1, title: '범죄도시4', years:'2023', country: '한국', reservationRate: '36%', audience: '100만' },
-        { src: movie2, title: '쿵푸팬더4', years:'2023', country: '한국', reservationRate: '36%', audience: '200만' },
-        { src: movie3, title: '혹성탈출(새로운 시대)', years:'2023', country: '한국', reservationRate: '36%', audience: '300만' },
-        { src: movie4, title: '포켓몬스터(성도지방 이야기, 최종장)', years:'2023', country: '한국', reservationRate: '36%', audience: '400만' },
-        { src: movie5, title: '스턴트맨', years:'2023', country: '한국', reservationRate: '36%', audience: '500만' },
-        { src: movie6, title: '악마와의 토크쇼', years:'2023', country: '한국', reservationRate: '36%', audience: '600만' },
-        { src: movie7, title: '가필드 더 무비', years:'2023', country: '한국', reservationRate: '36%', audience: '700만' },
-        { src: movie8, title: '하이큐 극장판(쓰레기장의 결전)', years:'2023', country: '한국', reservationRate: '36%', audience: '800만' },
-        { src: movie9, title: '꼬마참새 리차드(신비한 보석 탐험대)', years:'2023', country: '한국', reservationRate: '36%', audience: '900만' },
+const Series = () => {
+    const webtoonImg = [
+        { src: webtoonImg1, title: '나 혼자만 레벨업', years:'2023', country: '한국', reservationRate: '36%', audience: '100만' },
+        { src: webtoonImg2, title: '휘영검전', years:'2023', country: '한국', reservationRate: '36%', audience: '200만' },
+        { src: webtoonImg3, title: '아기님 캐시로 로판 달린다', years:'2023', country: '한국', reservationRate: '36%', audience: '300만' },
+        { src: webtoonImg4, title: '던전 리셋', years:'2023', country: '한국', reservationRate: '36%', audience: '400만' },
+        { src: webtoonImg5, title: '시그리드', years:'2023', country: '한국', reservationRate: '36%', audience: '500만' },
+        { src: webtoonImg6, title: '만렙 영웅님께서 귀환하신다!', years:'2023', country: '한국', reservationRate: '36%', audience: '600만' },
+        { src: webtoonImg7, title: '로드 오브 머니', years:'2023', country: '한국', reservationRate: '36%', audience: '700만' },
+        { src: webtoonImg8, title: '검술명가 막내아들', years:'2023', country: '한국', reservationRate: '36%', audience: '800만' },
+        { src: webtoonImg9, title: '토끼와 흑표범의 공생관계', years:'2023', country: '한국', reservationRate: '36%', audience: '900만' },
     ];
 
     return (
         <>
             <div className='titleName'>박스오피스 순위</div>
             <div className="movieList">
-                {movies.map((movie, index) => (
+                {webtoonImg.map((movie, index) => (
                     <div key={index} className='movieItem'>
                         <img src={movie.src} alt={movie.title} />
                         <div className='content1'> {movie.title}</div>
@@ -39,7 +39,7 @@ const Webtoon = () => {
             
             <div className='titleName'>공개 예정작</div>
             <div className="movieList">
-                {movies.map((movie, index) => (
+                {webtoonImg.map((movie, index) => (
                     <div key={index} className='movieItem'>
                         <img src={movie.src} alt={movie.title} />
                         <div className='content1'> {movie.title}</div>
@@ -51,7 +51,7 @@ const Webtoon = () => {
 
             <div className='titleName'>왓챠 구매 순위</div>
             <div className="movieList">
-                {movies.map((movie, index) => (
+                {webtoonImg.map((movie, index) => (
                     <div key={index} className='movieItem'>
                         <img src={movie.src} alt={movie.title} />
                         <div className='content1'> {movie.title}</div>
@@ -64,4 +64,4 @@ const Webtoon = () => {
     );
 };
 
-export default Webtoon;
+export default Series;
