8a9bcb7d35d2293b873def0925af08aba3fd60d3
youminki <dbalsrl7648@naver.com>
2024-05-08 15:13:20 +0900

    app.js 수정
    Header를 누르면 해당하는 메뉴바의 이미지가 보여지도록 변경

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
+import { useState } from 'react';
 import Footer from './page/Footer/Footer.js';
 import Header from './page/Header/Header.js';
 import Movie from './page/Movie.js';
 
-
 function App() {
+  const [showMovie, setShowMovie] = useState(true);
+
+  const handleHeaderClick = () => {
+    setShowMovie(false);
+  };
+
   return (
     <>
-      <Header />
-      <Movie />
+      <div onClick={handleHeaderClick}>
+        <Header />
+      </div>
+      {showMovie && <Movie />}
       <Footer />
     </>
   );
